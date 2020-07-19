146e3dd3f979147319c83ac5287935be03cfef2b
yshimaa66 <yshimaa6696@gmail.com>
2020-07-19 20:27:11 +0200

    first commit

--- app/src/main/java/com/example/weatherapp/CityActivity.java
+++ app/src/main/java/com/example/weatherapp/CityActivity.java
@@ -1,27 +1,60 @@
 package com.example.weatherapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class CityActivity extends AppCompatActivity {
 
+
+    protected TextView locationTextView;
     private Button gobtn;
     private EditText cityEditText;
 
+
+    int PERMISSION_ID = 44;
+    FusedLocationProviderClient mFusedLocationClient;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_city);
+        super.setContentView(R.layout.activity_city);
 
-        gobtn=findViewById(R.id.gobtn);
-        cityEditText=findViewById(R.id.cityEditText);
-
+        gobtn = findViewById(R.id.gobtn);
+        cityEditText = findViewById(R.id.cityEditText);
 
 
         gobtn.setOnClickListener(new View.OnClickListener() {
@@ -31,20 +64,17 @@ public class CityActivity extends AppCompatActivity {
 
                 String city = cityEditText.getText().toString();
 
-                if(city.matches("")){
+                if (city.matches("")) {
                     cityEditText.setError("Enter the city name");
-                }
+                } else {
 
-                else{
+                    Intent intent = new Intent(CityActivity.this, MainActivity.class);
 
-                Intent intent = new Intent(CityActivity.this,MainActivity.class);
+                    intent.putExtra("city", city);
 
-                intent.putExtra("city",city);
+                    //Toast.makeText(CityActivity.this,city+"00",Toast.LENGTH_LONG).show();
 
-                //Toast.makeText(CityActivity.this,city+"00",Toast.LENGTH_LONG).show();
-
-                CityActivity.this.startActivity(intent);
-
+                    CityActivity.this.startActivity(intent);
 
 
                 }
@@ -52,5 +82,166 @@ public class CityActivity extends AppCompatActivity {
 
             }
         });
+        initView();
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        getLastLocation();
+
+
+    }
+
+
+
+    private boolean checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(CityActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(CityActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        return false;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(
+                CityActivity.this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSION_ID
+        );
+    }
+
+
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+        );
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @SuppressLint("MissingPermission")
+    private void getLastLocation(){
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(
+                        new OnCompleteListener<Location>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Location> task) {
+                                Location location = task.getResult();
+                                if (location == null) {
+                                    requestNewLocationData();
+                                } else {
+
+
+                                    Geocoder geocoder = new Geocoder(CityActivity.this, Locale.getDefault());
+                                    List<Address> addresses = null;
+                                    try {
+                                        addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                    String cityName = addresses.get(0).getAddressLine(0);
+                                    String stateName = addresses.get(0).getAddressLine(1);
+                                    String countryName = addresses.get(0).getAddressLine(2);
+
+                                    String[] arrOfStr = cityName.split(",");
+
+
+                                   locationTextView.setText("You're in "+arrOfStr[arrOfStr.length-2]+", "+arrOfStr[arrOfStr.length-1]);
+
+                                   String cityinedittext= arrOfStr[arrOfStr.length-2].replace("Governorate","");
+
+                                    cityEditText.setText(cityinedittext.trim());
+                                }
+                            }
+                        }
+                );
+            } else {
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            requestPermissions();
+        }
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (checkPermissions()) {
+            getLastLocation();
+        }
+
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData(){
+
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(0);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.requestLocationUpdates(
+                mLocationRequest, mLocationCallback,
+                Looper.myLooper()
+        );
+
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            Geocoder geocoder = new Geocoder(CityActivity.this, Locale.getDefault());
+            List<Address> addresses = null;
+            try {
+                addresses = geocoder.getFromLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude(), 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String cityName = addresses.get(0).getAddressLine(0);
+            String stateName = addresses.get(0).getAddressLine(1);
+            String countryName = addresses.get(0).getAddressLine(2);
+
+            locationTextView.setText( countryName+ " " + cityName);
+            cityEditText.setText(cityName);
+
+        }
+
+
+
+    };
+
+
+
+
+
+
+
+
+    private void initView() {
+        cityEditText = (EditText) findViewById(R.id.cityEditText);
+        gobtn = (Button) findViewById(R.id.gobtn);
+        locationTextView = (TextView) findViewById(R.id.locationTextView);
     }
 }
